43acaffaea43b3d03fcdabf63cf374d59e1d5bfa
Awe Oluwatimileyin Matthew <105941839+CodeATM@users.noreply.github.com>
2025-09-17 13:10:41 +0100

    first commit

--- Link-list/intro.js
+++ Link-list/intro.js
@@ -1,30 +1,115 @@
-// Single Linked List
-
 // ================== Node =================//
-
 class MyNode {
   constructor(value) {
-    this.head = value;
+    this.value = value;
     this.next = null;
   }
 }
 
 class MyLinkedList {
   constructor(value) {
-    this.head = new MyNode(value);
-    this.tail = this.head;
+    const newNode = new MyNode(value);
+    this.head = newNode;
+    this.tail = newNode;
     this.length = 1;
   }
 
   push(value) {
-    let newNode = new MyNode(value);
-    this.tail.next = newNode; 
+    const newNode = new MyNode(value);
+
+    this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   }
+
+  pop() {
+    if (!this.head) {
+      prev = temp;
+      temp = prev.next;
+    }
+
+    let temp = this.head;
+    let prev = this.head;
+
+    while (temp.next) {
+      prev = temp;
+      temp = prev.next;
+    }
+
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+
+  unshift(value) {
+    const newNode = new MyNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let temp = this.head;
+
+    this.head = this.head.next;
+
+    temp.next = null;
+
+    this.length--;
+
+    if (this.length == 0) {
+      this.tail = null;
+    }
+
+    return temp;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let temp = this.head;
+
+    while (temp) {
+      if (!temp.next) {
+        return temp;
+      }
+
+      temp = temp.next;
+    }
+  }
 }
 
+// Create a new linked list and push a value
 const myNewList = new MyLinkedList(1);
-myNewList.push(10);
+myNewList.push(2);
+// myNewList.push(70);
+// myNewList.unshift(30);
+// myNewList.shift();
 
-console.log(myNewList);
+// myNewList.getLast();
+
+console.log(myNewList.getLast());
